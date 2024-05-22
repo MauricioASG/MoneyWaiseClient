766dd956e4ebdefcd844a7e144bdba45ad27b54d
Mauricio Alejandro Serrano García <mauricioalejandroserranogarcia@gmail.com>
2024-05-21 23:30:26 -0600

    First calendar desing added

--- android/app/src/screens/ExpenseSchedule.tsx
+++ android/app/src/screens/ExpenseSchedule.tsx
@@ -1,11 +1,12 @@
-// ScheduleScreen.tsx
 /* eslint-disable prettier/prettier */
+//EpenseSchedule
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView } from 'react-native';
 import FooterMenu from '../components/FooterMenu';
 import { StackNavigationProp } from '@react-navigation/stack';
 import { RouteProp, useFocusEffect } from '@react-navigation/native';
 import { useButton } from '../contexts/FooterMenuContext';
+import { Calendar } from 'react-native-calendars';
 
 type RootStackParamList = {
   Home: undefined;
@@ -29,24 +30,85 @@ const ScheduleScreen: React.FC<ScheduleScreenProps> = ({ navigation }) => {
   );
 
   return (
-    <View style={styles.container}>
-      <Text style={styles.heading}>Horario de Gastos</Text>
-      <FooterMenu navigation={navigation} />
-    </View>
+    <SafeAreaView style={styles.safeArea}>
+      <View style={styles.container}>
+        <Text style={styles.heading}>Calendario de gastos</Text>
+        <View style={styles.calendarContainer}>
+          <Calendar
+            onDayPress={(day) => {
+              console.log('selected day', day);
+            }}
+            monthFormat={'yyyy MM'}
+            hideExtraDays={true}
+            showWeekNumbers={true}
+            theme={{
+              backgroundColor: '#ffffff',
+              calendarBackground: '#ffffff',
+              textSectionTitleColor: '#b6c1cd',
+              selectedDayBackgroundColor: '#00adf5',
+              selectedDayTextColor: '#ffffff',
+              todayTextColor: '#00adf5',
+              dayTextColor: '#2d4150',
+              textDisabledColor: '#d9e1e8',
+              dotColor: '#00adf5',
+              selectedDotColor: '#ffffff',
+              arrowColor: 'orange',
+              monthTextColor: 'blue',
+              indicatorColor: 'blue',
+              textDayFontFamily: 'monospace',
+              textMonthFontFamily: 'monospace',
+              textDayHeaderFontFamily: 'monospace',
+              textDayFontWeight: '300',
+              textMonthFontWeight: 'bold',
+              textDayHeaderFontWeight: '300',
+              textDayFontSize: 16,
+              textMonthFontSize: 16,
+              textDayHeaderFontSize: 16,
+            }}
+            style={styles.calendar}
+          />
+        </View>
+        <FooterMenu navigation={navigation} />
+      </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
+  safeArea: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
+    justifyContent: 'flex-start',
     alignItems: 'center',
     padding: 20,
   },
   heading: {
     fontSize: 24,
     fontWeight: 'bold',
+    color: '#333',
     marginBottom: 20,
+  },
+  calendarContainer: {
+    width: '90%', // Ajustar el ancho del contenedor del calendario
+    backgroundColor: '#fff',
+    borderRadius: 10,
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.25,
+    shadowRadius: 3.84,
+    elevation: 5,
+    padding: 10,
+    marginVertical: 20,
+    alignSelf: 'center',
+  },
+  calendar: {
+    borderRadius: 10,
   },
 });
 
