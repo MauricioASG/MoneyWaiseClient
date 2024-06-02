98865a5d975b0aef6aceb5835def85f0ef302d78
Mauricio Alejandro Serrano García <mauricioalejandroserranogarcia@gmail.com>
2024-06-01 23:13:08 -0600

    WebServiceParams.ts created

---
+++ android/app/src/WebServiceParams.ts
@@ -0,0 +1,8 @@
+/* eslint-disable prettier/prettier */
+
+const host = '172.29.224.1';
+const port = 3050;
+
+export default {host, port};
+
+// http://${WebServiceParams.host}:${WebServiceParams.port}/all-recent`
