7426ec56847edfb64317347115c785dec1f4413c
Mauricio Alejandro Serrano García <mauricioalejandroserranogarcia@gmail.com>
2024-09-01 17:57:53 -0600

    Change in the categories and types of transactions in the pickers when creating movements

--- android/app/src/screens/AddTransactionScreen.tsx
+++ android/app/src/screens/AddTransactionScreen.tsx
@@ -24,11 +24,95 @@ type AddTransactionProps = {
 const AddTransactionScreen: React.FC<AddTransactionProps> = ({ navigation, route }) => {
   const { userId } = useContext(UserContext);
   const [amount, setAmount] = useState('');
-  const [type, setType] = useState('');  // No se asigna ningún valor por defecto
-  const [categoryId, setCategoryId] = useState(''); // Usamos el ID de categoría
+  const [categoryId, setCategoryId] = useState('0'); // Default to auxiliary category
+  const [type, setType] = useState('Selecciona un tipo de gasto'); // Default to auxiliary type
+
+  // Mapa de tipos de subcategorías por categoría
+  const categoryTypes = {
+    '1': [
+      { label: 'Alquiler/Hipoteca', value: 'Vivienda_Alquiler/Hipoteca' },
+      { label: 'Servicios básicos', value: 'Vivienda_Servicios básicos' },
+      { label: 'Mantenimiento', value: 'Vivienda_Mantenimiento' },
+      { label: 'Impuestos inmobiliarios', value: 'Vivienda_Impuestos inmobiliarios' },
+      { label: 'Seguros de hogar', value: 'Vivienda_Seguros de hogar' },
+    ],
+    '2': [
+      { label: 'Combustible', value: 'Transporte_Combustible' },
+      { label: 'Transporte público', value: 'Transporte_Transporte público' },
+      { label: 'Mantenimiento del vehículo', value: 'Transporte_Mantenimiento del vehículo' },
+      { label: 'Peajes', value: 'Transporte_Peajes' },
+      { label: 'Estacionamiento', value: 'Transporte_Estacionamiento' },
+      { label: 'Seguro del vehículo', value: 'Transporte_Seguro del vehículo' },
+    ],
+    '3': [
+      { label: 'Supermercado', value: 'Alimentación_Supermercado' },
+      { label: 'Restaurantes', value: 'Alimentación_Restaurantes' },
+      { label: 'Comida rápida', value: 'Alimentación_Comida rápida' },
+      { label: 'Bebidas', value: 'Alimentación_Bebidas' },
+      { label: 'Snacks', value: 'Alimentación_Snacks' },
+    ],
+    '4': [
+      { label: 'Consultas médicas', value: 'Salud_Consultas médicas' },
+      { label: 'Medicamentos', value: 'Salud_Medicamentos' },
+      { label: 'Seguro médico', value: 'Salud_Seguro médico' },
+      { label: 'Gimnasio', value: 'Salud_Gimnasio' },
+      { label: 'Productos de belleza', value: 'Salud_Productos de belleza' },
+      { label: 'Cuidado personal', value: 'Salud_Cuidado personal' },
+    ],
+    '5': [
+      { label: 'Colegiaturas', value: 'Educación_Colegiaturas' },
+      { label: 'Libros y materiales', value: 'Educación_Libros y materiales' },
+      { label: 'Cursos y talleres', value: 'Educación_Cursos y talleres' },
+      { label: 'Material escolar', value: 'Educación_Material escolar' },
+      { label: 'Actividades extracurriculares', value: 'Educación_Actividades extracurriculares' },
+    ],
+    '6': [
+      { label: 'Cine', value: 'Entretenimiento_Cine' },
+      { label: 'Teatro', value: 'Entretenimiento_Teatro' },
+      { label: 'Conciertos', value: 'Entretenimiento_Conciertos' },
+      { label: 'Viajes', value: 'Entretenimiento_Viajes' },
+      { label: 'Suscripciones', value: 'Entretenimiento_Suscripciones' },
+      { label: 'Hobbies', value: 'Entretenimiento_Hobbies' },
+    ],
+    '7': [
+      { label: 'Vestimenta', value: 'Ropa y calzado_Vestimenta' },
+      { label: 'Calzado', value: 'Ropa y calzado_Calzado' },
+      { label: 'Accesorios', value: 'Ropa y calzado_Accesorios' },
+    ],
+    '8': [
+      { label: 'Cumpleaños', value: 'Regalos_Cumpleaños' },
+      { label: 'Navidad', value: 'Regalos_Navidad' },
+      { label: 'Otras ocasiones', value: 'Regalos_Otras ocasiones' },
+    ],
+    '9': [
+      { label: 'Ahorro a corto plazo', value: 'Ahorro e inversión_Ahorro a corto plazo' },
+      { label: 'Ahorro a largo plazo', value: 'Ahorro e inversión_Ahorro a largo plazo' },
+      { label: 'Inversiones', value: 'Ahorro e inversión_Inversiones' },
+    ],
+    '10': [
+      { label: 'Tarjeta de crédito', value: 'Deudas_Tarjeta de crédito' },
+      { label: 'Préstamos personales', value: 'Deudas_Préstamos personales' },
+      { label: 'Otros préstamos', value: 'Deudas_Otros préstamos' },
+    ],
+    '11': [
+      { label: 'Imprevistos', value: 'Otros_Imprevistos' },
+      { label: 'Donaciones', value: 'Otros_Donaciones' },
+      { label: 'Impuestos (otros que no sean inmobiliarios)', value: 'Otros_Impuestos (otros que no sean inmobiliarios)' },
+    ],
+    '12': [
+      { label: 'Alimento', value: 'Mascotas_Alimento' },
+      { label: 'Veterinaria', value: 'Mascotas_Veterinaria' },
+      { label: 'Productos de higiene', value: 'Mascotas_Productos de higiene' },
+      { label: 'Accesorios', value: 'Mascotas_Accesorios' },
+      { label: 'Adiestramiento', value: 'Mascotas_Adiestramiento' },
+    ],
+    '0': [
+      { label: 'Selecciona un tipo de gasto', value: 'Selecciona un tipo de gasto' },
+    ]
+  };
 
   const handleAddTransaction = async () => {
-    if (!amount || isNaN(Number(amount)) || !categoryId || !type) {
+    if (!amount || isNaN(Number(amount)) || categoryId === '0' || type === 'Selecciona un tipo de gasto') {
       Alert.alert('Error', 'Por favor, ingrese una cantidad válida, seleccione una categoría y un tipo de transacción');
       return;
     }
@@ -59,35 +143,44 @@ const AddTransactionScreen: React.FC<AddTransactionProps> = ({ navigation, route
       />
       <Picker
         selectedValue={categoryId}
-        onValueChange={(itemValue) => setCategoryId(itemValue)}
+        onValueChange={(itemValue) => {
+          setCategoryId(itemValue);
+          setType('Selecciona un tipo de gasto'); // Reset type when category changes
+        }}
         style={styles.picker}
       >
-        <Picker.Item label="Selecciona categoría" value="" />
-        <Picker.Item label="Comida" value="1" />
-        <Picker.Item label="Transporte" value="3" />
-        <Picker.Item label="Entretenimiento" value="4" />
-        {/* Agrega más categorías según corresponda */}
+        <Picker.Item label="Selecciona categoría" value="0" />
+        <Picker.Item label="Vivienda" value="1" />
+        <Picker.Item label="Transporte" value="2" />
+        <Picker.Item label="Alimentación" value="3" />
+        <Picker.Item label="Salud" value="4" />
+        <Picker.Item label="Educación" value="5" />
+        <Picker.Item label="Entretenimiento" value="6" />
+        <Picker.Item label="Ropa y calzado" value="7" />
+        <Picker.Item label="Regalos" value="8" />
+        <Picker.Item label="Ahorro e inversión" value="9" />
+        <Picker.Item label="Deudas" value="10" />
+        <Picker.Item label="Otros" value="11" />
+        <Picker.Item label="Mascotas" value="12" />
       </Picker>
+
       <Picker
         selectedValue={type}
         onValueChange={(itemValue) => setType(itemValue)}
         style={styles.picker}
+        enabled={categoryId !== '0'} // Disable if no valid category is selected
       >
-        <Picker.Item label="Selecciona tipo" value="" />
-        <Picker.Item label="Ingreso" value="Ingreso" />
-        <Picker.Item label="Gasto" value="Gasto" />
-        <Picker.Item label="Prioritario" value="Prioritario" />
-        <Picker.Item label="Recreativo" value="Recreativo" />
-        <Picker.Item label="Hormiga" value="Hormiga" />
-        <Picker.Item label="Servicios" value="Servicios" />
+        {categoryTypes[categoryId]?.map((typeItem) => (
+          <Picker.Item key={typeItem.value} label={typeItem.label} value={typeItem.value} />
+        ))}
       </Picker>
+
       <TouchableOpacity style={styles.button} onPress={handleAddTransaction}>
         <Text style={styles.buttonText}>Agregar</Text>
       </TouchableOpacity>
     </SafeAreaView>
   );
 };
-
 
 const styles = StyleSheet.create({
   container: {
