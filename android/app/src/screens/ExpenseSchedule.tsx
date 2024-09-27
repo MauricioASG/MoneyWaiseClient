306151c87c57ddaa7712bd459bb769a5aa15905d
MauricioSG <mauricioalejandroserranogarcia@gmail.com>
2024-09-27 17:21:18 -0600

    Modifications and improvements to the calendar

--- android/app/src/screens/ExpenseSchedule.tsx
+++ android/app/src/screens/ExpenseSchedule.tsx
@@ -8,8 +8,7 @@ import { RouteProp, useFocusEffect } from '@react-navigation/native';
 import { useButton } from '../contexts/FooterMenuContext';
 import { Calendar } from 'react-native-calendars';
 import { UserContext } from '../contexts/UserContext';
-import { getTransactionsByDate } from '../api';
-
+import { getTransactionsByMonth } from '../api';
 
 type RootStackParamList = {
   Home: undefined;
@@ -28,35 +27,86 @@ type ScheduleScreenProps = {
 const ScheduleScreen: React.FC<ScheduleScreenProps> = ({ navigation }) => {
   const { setSelectedButton } = useButton();
   const { userId } = useContext(UserContext);
+
   const [selectedDate, setSelectedDate] = useState('');
   const [transactions, setTransactions] = useState([]);
+  const [markedDates, setMarkedDates] = useState({});
+  const [currentMonth, setCurrentMonth] = useState(new Date().getMonth() + 1); // Los meses en JS van de 0 a 11
+  const [currentYear, setCurrentYear] = useState(new Date().getFullYear());
+  const [transactionsByDate, setTransactionsByDate] = useState({});
 
   useFocusEffect(
     React.useCallback(() => {
       setSelectedButton('right');
-      if (selectedDate) {
-        fetchTransactions(selectedDate);
-      }
-    }, [setSelectedButton, selectedDate])
+      fetchMonthlyTransactions();
+    }, [setSelectedButton, currentMonth, currentYear])
   );
 
-  const fetchTransactions = async (date: string) => {
+  const fetchMonthlyTransactions = async () => {
     try {
-      const data = await getTransactionsByDate(userId, date);
-      setTransactions(data);
+      const data = await getTransactionsByMonth(userId, currentYear, currentMonth);
+      const groupedTransactions = {};
+
+      data.forEach((transaction) => {
+        const date = transaction.fecha.split('T')[0]; // Obtener solo la parte de la fecha
+        if (!groupedTransactions[date]) {
+          groupedTransactions[date] = [];
+        }
+        groupedTransactions[date].push(transaction);
+      });
+
+      setTransactionsByDate(groupedTransactions);
     } catch (error) {
-      console.error('Error fetching transactions:', error);
+      console.error('Error fetching monthly transactions:', error);
     }
-};
+  };
+
+  useEffect(() => {
+    const newMarkedDates = {};
+
+    // Marcar fechas con transacciones
+    Object.keys(transactionsByDate).forEach((date) => {
+      newMarkedDates[date] = { marked: true, dotColor: 'red' };
+    });
+
+    // Marcar la fecha seleccionada
+    if (selectedDate) {
+      newMarkedDates[selectedDate] = {
+        ...(newMarkedDates[selectedDate] || {}),
+        selected: true,
+        selectedColor: '#00adf5',
+      };
+    }
+
+    setMarkedDates(newMarkedDates);
+  }, [transactionsByDate, selectedDate]);
+
+  useEffect(() => {
+    if (selectedDate) {
+      const transactionsForDate = transactionsByDate[selectedDate] || [];
+      setTransactions(transactionsForDate);
+    } else {
+      setTransactions([]);
+    }
+  }, [selectedDate, transactionsByDate]);
+
+  const onDayPress = (day) => {
+    setSelectedDate(day.dateString);
+  };
+
+  const onMonthChange = (month) => {
+    setCurrentMonth(month.month);
+    setCurrentYear(month.year);
+  };
 
   return (
     <SafeAreaView style={styles.safeArea}>
       <View style={styles.container}>
         <View style={styles.calendarContainer}>
           <Calendar
-            onDayPress={(day) => {
-              setSelectedDate(day.dateString);
-            }}
+            onDayPress={onDayPress}
+            onMonthChange={onMonthChange}
+            markedDates={markedDates}
             monthFormat={'yyyy MM'}
             hideExtraDays={true}
             showWeekNumbers={true}
@@ -173,7 +223,6 @@ const styles = StyleSheet.create({
     marginTop: 10,
     marginBottom: 20,
     alignItems: 'center',
-    
   },
   viewAllText: {
     color: '#00adf5',
@@ -187,4 +236,3 @@ const styles = StyleSheet.create({
 });
 
 export default ScheduleScreen;
-
